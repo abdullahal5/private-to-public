b1c15436f660829a703db99f87c49587c68d2caa
abdullahal5 <abdullahalfahin183@gmail.com>
2023-11-25 00:08:47 -0800

    firebase authprovider

---
+++ src/Components/firebase/firebase.cocfig.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDvTScieEt7xtMU4Xt6Uy4gCQD-CCWrggU",
+  authDomain: "assignment12-24a39.firebaseapp.com",
+  projectId: "assignment12-24a39",
+  storageBucket: "assignment12-24a39.appspot.com",
+  messagingSenderId: "889945346470",
+  appId: "1:889945346470:web:795324e18b68a73db73f67",
+};
+
+export const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,18 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import AuthProvider from './Components/Provider/AuthProvider';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Container>
-        <RouterProvider router={router} />
-      </Container>
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Container>
+          <RouterProvider router={router} />
+        </Container>
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
